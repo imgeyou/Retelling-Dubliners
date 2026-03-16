c4385ccc45945d7335244ddf36c8cd646c6e1ab8
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-16 20:34:45 +0000

    editing toggle JS

--- app/js/script.js
+++ app/js/script.js
@@ -11,9 +11,22 @@ document.querySelectorAll('.slider').forEach(slider => {
 const body = document.querySelector("body");
 toggle = document.querySelector(".toggle");
 
-
-toggle.addEventListener("click", () =>{
+//setting what color mode it's in
+let getMode = localStorage.getItem("mode");
+console.log(getMode);
+if (getMode && getMode === "dark"){
     body.classList.toggle("dark");
+    toggle.classList.toggle("active")
+}
+
+//toggling light vs dark mode
+toggle.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (!body.classList.contains("dark")) {
+        return localStorage.setItem("mode", "light");
+    }
+    localStorage.setItem("mode", "dark");
 })
 
 // activating toggle on click
