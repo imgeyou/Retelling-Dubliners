d94a97a14bb22a36f1eeed04df5b467bef230313
Logan Dulski <96384843+ldulski@users.noreply.github.com>
2026-03-16 20:14:56 +0000

    adding light+dark mode toggle in HTML,CSS, and JS

--- app/js/script.js
+++ app/js/script.js
@@ -7,60 +7,44 @@ document.querySelectorAll('.slider').forEach(slider => {
     });
 })
 
-// check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode');
+//light mode + dark mode toggle
+const body = document.querySelector("body");
+toggle = document.querySelector(".toggle");
 
-const darkModeToggle = document.querySelector('#theme-switch');
+// activating toggle on click
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
 
-const enableDarkMode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkMode', 'enabled');
-}
 
-const disableDarkMode = () => {
-    console.log("Running Disable Function")
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkMode', null);
-}
 
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (darkMode === 'enabled') {
-    enableDarkMode();
-}
 
-// When someone clicks the button
-darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem('darkMode');
-    console.log(darkMode)
-    if (darkMode != 'enabled') {
-        console.log("Enabling");
-        enableDarkMode();
-    } else {
-        console.log("Disabling");
-        disableDarkMode();
-    }
-});
+
+
+
+
+
+
+
+
 
 //life of jj quotes swapping section
 // quote array begins here
-const quotes = [
-    "Life is too short to read a bad book — James Joyce",
-    "Why is it that words like these seem dull and cold? Is it because there is no word tender enough to be your name?― James Joyce, The Dead",
-    "Better pass boldly into that other world, in the full glory of some passion, than fade and wither dismally with age — James Joyce",
-    "I want to give a picture of Dublin so complete that if the city suddenly disappeared from the earth it could be reconstructed out of my book — James Joyce said in conversation with Frank Budgen, 1918",
-    "Joyce is right about history being a nightmare — but it may be the nightmare from which no one can awaken. People are trapped in history and history is trapped in them. – James Baldwin, Notes of a Native Son about James Joyce",
-];
+// const quotes = [
+//     "Life is too short to read a bad book — James Joyce",
+//     "Why is it that words like these seem dull and cold? Is it because there is no word tender enough to be your name?― James Joyce, The Dead",
+//     "Better pass boldly into that other world, in the full glory of some passion, than fade and wither dismally with age — James Joyce",
+//     "I want to give a picture of Dublin so complete that if the city suddenly disappeared from the earth it could be reconstructed out of my book — James Joyce said in conversation with Frank Budgen, 1918",
+//     "Joyce is right about history being a nightmare — but it may be the nightmare from which no one can awaken. People are trapped in history and history is trapped in them. – James Baldwin, Notes of a Native Son about James Joyce",
+// ];
 
 //Swapping quotes
-function randomQuote() {
-    const quoteSpan = document.getElementById("quote");
-    if (quoteSpan) {
-        quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-    }
-}
+// function randomQuote() {
+//     const quoteSpan = document.getElementById("quote");
+//     if (quoteSpan) {
+//         quoteSpan.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+//     }
+// }
 //Quote Time Intervals
-randomQuote();
-window.setInterval(randomQuote, 5000);
+// randomQuote();
+// window.setInterval(randomQuote, 5000);
 
 
