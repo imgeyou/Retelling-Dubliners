a7e28bcfdcb9dc2d3ccbb3f70258f534960b2671
tianyisunn <tianyi_sun0611@163.com>
2026-03-25 10:58:11 +0000

    js loadmore btn solved

--- app/js/loadmore.js
+++ app/js/loadmore.js
@@ -1,26 +1,36 @@
 console.log('I load more cards!')
 
 //load more
-let loadMoreBtn = document.querySelector('#load-more');
+let loadMoreBtn = document.querySelector('.loadmore');
 //number of cards that should appear on load
-let currentItem = 4;
+let currentItem = 9;
 
 //tester code 1
 // console.log(loadMoreBtn);
 // console.log(currentItem);
 
+let boxes = [...document.querySelectorAll('.locationCardContainer, .card')];
+//hide the rest other than first 8
+boxes.forEach((box, index) => {
+    if (index >= currentItem) {
+        box.style.display = 'none';
+    }
+});
 
 //on buttom click more card will load in intances of 4
 loadMoreBtn.onclick = () => {
     let boxes = [...document.querySelectorAll('.locationCardContainer, .card')];
-    for (var i = currentItem; i < currentItem + 4; i++) {
-        boxes[i].style.display = 'inline-block';
-        //tester code 2
-        console.log('I work!');
+    for (var i = currentItem; i < currentItem + 16; i++) {
+        if(boxes[i]){
+            boxes[i].style.display = 'inline-block';
+            boxes[i].classList.add('card-reveal'); 
+            //tester code 2
+            console.log('I work!');
+        }
     }
-    currentItem += 4;
+    currentItem += 16;
     // load more button will disappear when all cards run out
     if (currentItem >= boxes.length) {
-        boxes[i].style.display = 'none';
+        loadMoreBtn.style.display = 'none';
     }
 }
